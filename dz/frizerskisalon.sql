9da99cbf47af566d4fe88981202e56b2c8679d93
nadf <73925816+nadf@users.noreply.github.com>
2020-11-16 01:29:55 +0100

    prvi zadatak

---
+++ dz/frizerskisalon.sql
@@ -0,0 +1,42 @@
+#1. Frizerski salon
+# win+r, upisi cmd te zalijepi sljedecu liniju (prilagoditi putanje - diskove)
+# c:\xampp\mysql\bin\mysql -uedunova -pedunova < e:\frizerskisalon.sql
+drop database if exists frizerskisalon;
+create database frizerskisalon;
+
+use frizerskisalon;
+
+create table djelatnica(
+    sifra int not null primary key auto_increment,
+    osoba int not null,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    iban varchar(50)
+);
+
+create table osoba(
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    oib char(11)
+);
+
+create table usluga(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    cijena decimal(18,2)
+);
+
+create table posjeta(
+    sifra int not null primary key auto_increment,
+    korisnik int not null,
+    djelatnica int not null,
+    usluga int not null
+);
+
+
+alter table djelatnica add foreign key (osoba) references osoba(sifra);
+
+alter table posjeta add foreign key (korisnik) references osoba(sifra);
+alter table posjeta add foreign key (djelatnica) references djelatnica(sifra);
+alter table posjeta add foreign key (usluga) references usluga(sifra);
