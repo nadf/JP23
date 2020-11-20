bf11dace367104eefe0a6c87c1ff9a48ef5149bd
nadf <73925816+nadf@users.noreply.github.com>
2020-11-21 00:16:03 +0100

    jedanaesti zadatak

---
+++ fakultet.sql
@@ -0,0 +1,43 @@
+# 11. Fakultet
+# Studenti polažu kolegije kroz rokove. 
+# Student može rok prijaviti više puta a na jedan rok može izaći više studenata.
+# win+r, upisi cmd te zalijepi sljedecu liniju (prilagoditi putanje - diskove)
+# c:\xampp\mysql\bin\mysql -uedunova -pedunova < e:\fakultet.sql
+drop database if exists  fakultet;
+create database fakultet;
+
+use fakultet;
+
+create table kolegij(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    pocetak datetime not null
+);
+
+create table rok(
+    sifra int not null primary key auto_increment,
+    datum datetime not null,
+    kolegij int not null
+);
+
+create table student(
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    oib char(11) not null,
+    kolegij int not null
+);
+
+create table student_rok(
+    sifra int not null primary key auto_increment,
+    student int not null,
+    rok int not null
+);
+
+
+alter table rok add foreign key (kolegij) references kolegij(sifra);
+
+alter table student_rok add foreign key (student) references student(sifra);
+alter table student_rok add foreign key (rok) references rok(sifra);
+
+alter table student add foreign key (kolegij) references kolegij(sifra);
